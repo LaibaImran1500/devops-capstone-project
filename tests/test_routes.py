6ca722222c472387485c9ca9ef7b1e0cf25bde96
LaibaImran1500 <157962750+LaibaImran1500@users.noreply.github.com>
2024-01-28 21:16:33 +0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -132,3 +132,8 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
+    
+  def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
