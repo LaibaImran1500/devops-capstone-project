784f3b4e6a589a796a606e83072c395f0821f770
LaibaImran1500 <157962750+LaibaImran1500@users.noreply.github.com>
2024-01-28 21:09:27 +0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -128,8 +128,7 @@ class TestAccountService(TestCase):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
-            f"{BASE_URL}/{account.id}", content_type="application/json"
-        )
+            f"{BASE_URL}/{account.id}", content_type="application/json")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
